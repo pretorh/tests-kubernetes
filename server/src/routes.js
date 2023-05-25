a97a606368f0a30b5b037322ea10fa2368358931
Hendri Pretorius <pretorh@gmail.com>
2023-05-25 17:49:52 +0200

    server: add helper function to send json data
    include the hostname, the server date and the message

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import { hostname } from 'os';
 import { logger } from './utils/logger.js';
 
 export const app = express();
 app.use(morgan('combined'));
 
-app.use('/', (req, res) => {
-  res.send('ok');
+function send(res, message) {
+  res.send({ host: hostname(), message, date: new Date() });
+}
+
+app.get('/', (req, res) => {
+  send(res, 'ok');
 });
 
 logger.debug('express app set up');
